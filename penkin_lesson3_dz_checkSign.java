1d3c68955e46cd16e9baa02d582012e555a2e8e4
penkinsergey <penkinsergeykms@gmail.com>
2023-10-24 22:14:17 +0800

    checkSign+selectColor

---
+++ penkin_lesson3_dz_checkSign.java
@@ -0,0 +1,19 @@
+package ru.java.penkin.lesson3dz;
+
+public class penkin_lesson3_dz_checkSign {
+    public static void checkSign() {
+        int a = 2;
+        int b = 5;
+        int c = 10;
+        int result = a + b + c;
+        if (result >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkSign();
+    }
+}
---
+++ penkin_lesson3_dz_selectColor.java
@@ -0,0 +1,17 @@
+package ru.java.penkin.lesson3dz;
+
+public class penkin_lesson3_dz_selectColor {
+    public static void selectColor() {
+        int data = 15;
+        if (data <= 10) {
+            System.out.println("Красный");
+        } else if (data < 20 && data > 10) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+    public static void main(String[] args) {
+            selectColor();
+        }
+    }
