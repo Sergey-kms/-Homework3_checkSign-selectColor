62d316adbb548483f7990b401f8adf6eee76e67f
penkinsergey <penkinsergeykms@gmail.com>
2023-10-24 23:14:42 +0800

    checkSign+selectColor+compareNumbers+Random

---
+++ penkin_lessom3_random.java
@@ -0,0 +1,8 @@
+package ru.java.penkin.lesson3dz;
+
+public class penkin_lessom3_random {
+    public static void main(String[] args) {
+        double r = Math.random();
+        System.out.println(r);
+    }
+}
