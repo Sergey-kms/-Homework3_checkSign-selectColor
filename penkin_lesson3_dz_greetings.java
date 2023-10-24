55c7d2475a46f5b31702aa256b6749a8a6de19e1
penkinsergey <penkinsergeykms@gmail.com>
2023-10-24 23:19:15 +0800

    less3_penkin_greetings+checkSign+selectColor+compareNumbers+Random

---
+++ penkin_lesson3_dz_greetings.java
@@ -0,0 +1,13 @@
+package ru.java.penkin.lesson3dz;
+
+public class penkin_lesson3_dz_greetings {
+    public static void greetings () {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+    public static void main (String[] args) {
+        greetings();
+    }
+}
